1f30cf3c3ca4b4304e3056ece3f5f84ab759fb9d
IShah27 <is2404@barnard.edu>
2014-10-08 21:03:26 -0400

    toggle button through browser compatibility

--- Week2StarTracker/chapter_02/01_document_ready/script.js
+++ Week2StarTracker/chapter_02/01_document_ready/script.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
 console.log("OMG JAVASCRIPT");
 	$("#celebs tbody tr:even").addClass("zebra");
 
-	$('#toggleButton')
-	
+
+	$('#toggleButton').click(function(){
+		$('#disclaimer').toggle();
+		if($('#disclaimer').is(':visible')){
+			$(this).val('hide');
+		}
+		else{
+			$(this).val('show')
+		}
+	})
+	$("<input type='button' id='toggleButton' value='toggle'/>").insertAfter('#disclaimer')
+	$('#toggleButton').click(function(){
+		$('#disclaimer').toggle();
+	});
+	$('<strong>START</strong>').prependTo('#disclaimer')
+	$('<strong>END</strong>').appendTo('#disclaimer')
 });
