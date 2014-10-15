d60231a1e9e0b717e56f5e4b074c40916d8fc41d
IShah27 <is2404@barnard.edu>
2014-10-15 19:53:07 -0400

    Fading out toggle button

--- Week2StarTracker/chapter_02/01_document_ready/script.js
+++ Week2StarTracker/chapter_02/01_document_ready/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 console.log("OMG JAVASCRIPT");
 	$("#celebs tbody tr:even").addClass("zebra");
 
-
 	$('#toggleButton').click(function(){
 		$('#disclaimer').toggle();
 		if($('#disclaimer').is(':visible')){
@@ -13,9 +12,6 @@ console.log("OMG JAVASCRIPT");
 		}
 	})
 	$("<input type='button' id='toggleButton' value='toggle'/>").insertAfter('#disclaimer')
-	$('#toggleButton').click(function(){
-		$('#disclaimer').toggle();
-	});
-	$('<strong>START</strong>').prependTo('#disclaimer')
-	$('<strong>END</strong>').appendTo('#disclaimer')
-});
+	$("#toggleButton").click(function(){
+		$("#disclaimer").fadeOut("20000");
+	})
